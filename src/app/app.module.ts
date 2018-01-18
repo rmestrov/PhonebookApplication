63ad38c5d8a5fc393dda1fa7a16a48753298ffc4
rmestrov <rmestrov@fesb.hr>
2018-01-18 18:54:33 +0100

    Added contact detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactsComponent
+    ContactsComponent,
+    ContactDetailComponent
   ],
   imports: [
     BrowserModule,
